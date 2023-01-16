49208b20a389508ea1e3cf675a6c6aca029ecf69
Arventill <filip.nowicki1999@gmail.com>
2023-01-16 19:51:40 +0100

    Add dbContext with a usage template for PostgreSQL

---
+++ AgileApp/AgileContext.cs
@@ -0,0 +1,13 @@
+﻿using AgileApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgileApp
+{
+    public class AgileContext : DbContext
+    {
+        public DbSet<User>? Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql(@"Host=localhost;Username=postgres;Password=student;Database=AgileDb");
+    }
+}
---
+++ AgileApp/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace AgileApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
