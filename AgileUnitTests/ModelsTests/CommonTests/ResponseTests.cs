48faf7b5eaba7b6169645ff66458125be44246f7
dawiddojczman <dawiddojczman@gmail.com>
2023-01-31 08:37:05 +0100

    Models xUnit tests

---
+++ AgileUnitTests/ModelsTests/CommonTests/ResponseTests.cs
@@ -0,0 +1,46 @@
+﻿using AgileApp.Models.Common;
+using Xunit;
+
+namespace AgileUnitTests.ModelsTests.CommonTests
+{
+    public class ResponseTests
+    {
+        [Fact]
+        public void ShouldReturnCorrectValue_WhenResponseIsSuccessful()
+        {
+            var response = Response.Succeeded();
+
+            Assert.True(response.IsSuccess);
+        }
+
+        [Fact]
+        public void ShouldReturnCorrectValue_WhenResponseFails()
+        {
+            var response = Response.Failed();
+
+            Assert.False(response.IsSuccess);
+        }
+    }
+
+    public class ResponseWithDataTests
+    {
+        [Fact]
+        public void ShouldReturnCorrectValues_WhenResponseIsSuccessful()
+        {
+            var data = "Test data";
+            var response = Response<string>.Succeeded(data);
+
+            Assert.True(response.IsSuccess);
+            Assert.Equal(data, response.Data);
+        }
+
+        [Fact]
+        public void ShouldReturnCorrectValues_WhenResponseFails()
+        {
+            var response = Response<string>.Failed();
+
+            Assert.False(response.IsSuccess);
+            Assert.Null(response.Data);
+        }
+    }
+}
---
+++ AgileUnitTests/ModelsTests/JwtTests/JwtReverseResultTests.cs
@@ -0,0 +1,33 @@
+﻿using AgileApp.Models.Jwt;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Xunit;
+
+namespace AgileUnitTests.ModelsTests.JwtTests
+{
+    public class JwtReverseResultTests
+    {
+        [Fact]
+        public void ShouldReturnCorrectValues_WhenResultIsInvalid()
+        {
+            var result = JwtReverseResult.Invalid();
+
+            Assert.False(result.IsValid);
+            Assert.Null(result.Claims);
+        }
+
+        [Fact]
+        public void ShouldReturnCorrectValues_WhenResultIsValid()
+        {
+            var claims = new List<Claim>
+            {
+                new Claim("TestType", "TestValue")
+            };
+
+            var result = JwtReverseResult.Valid(claims);
+
+            Assert.True(result.IsValid);
+            Assert.Equal(claims, result.Claims);
+        }
+    }
+}
