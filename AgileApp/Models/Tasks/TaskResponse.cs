f9cbdcd09a48f44169fad1767a968a3f5b6f1c90
Arventill <filip.nowicki1999@gmail.com>
2023-02-04 20:23:36 +0100

    Change task response name

--- AgileApp/Models/Tasks/TaskResponse.cs
+++ AgileApp/Models/Tasks/TaskResponse.cs
@@ -6,7 +6,7 @@ namespace AgileApp.Models.Tasks
     {
         public int Id { get; set; }
 
-        public string Title { get; set; }
+        public string Name { get; set; }
 
         public string Description { get; set; }
 
